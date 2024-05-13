ae674858a1358dd70a1f53f29c8ce7cd81a273b4
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-05-14 01:39:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const http = require("http");
 const port = 3000;
 
-// TODO: Create an HTTP server
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello, Faiz!\n");
 });
 
-// TODO: Listen on port 3000
+
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
